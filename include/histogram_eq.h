9e7398e9d66b777882b64bc27deb434e6ccbbcc1
claudiofcg <c.guerra@campus.fct.unl.pt>
2024-05-23 16:27:36 +0100

    Fixed CUDA.
    Remove useless code. Add new type.
    
    OpenMP implementation.
    
    Add original sequential function. Reformat code.
    
    Add OpenMP flags.
    
    Add test for original function as well.
    
    Add test for big file.
    
    Create output folder.
    
    Start to separate tests.

--- include/histogram_eq.h
+++ include/histogram_eq.h
@@ -3,8 +3,12 @@
 
 #include "wb.h"
 
-namespace cp {
-    wbImage_t iterative_histogram_equalization(wbImage_t &input_image, int iterations = 1);
+namespace cp
+{
+    enum class func_type { ORG, OMP, CUDA };
+
+    wbImage_t iterative_histogram_equalization(const wbImage_t& input_image, int iterations = 1,
+                                               func_type type = func_type::ORG);
 }
 
 #endif //CP_PROJECT_HISTOGRAM_EQ_H
