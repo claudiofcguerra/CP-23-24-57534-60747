4763a06c15cad54dc8e1928e423fc9956a3fcac5
claudiofcg <c.guerra@campus.fct.unl.pt>
2024-05-28 20:11:04 +0100

    Merge all tests together. Use correct CUDA file.

--- src/histogram_eq_cuda.cu
+++ src/histogram_eq_cuda.cu
@@ -1,67 +1,134 @@
-//
-// Created by herve on 13-04-2024.
-//
-
+#include <cub/cub.cuh>
 #include "histogram_eq.h"
 
 namespace cp
 {
     constexpr auto HISTOGRAM_LENGTH = 256;
 
-    static float prob(const int x, const int size)
+    __global__ void histogram_kernel(const unsigned char* gray_image, int size, int* histogram)
     {
-        return static_cast<float>(x) / static_cast<float>(size);
+        int tid = threadIdx.x + blockIdx.x * blockDim.x;
+        if (tid < size)
+        {
+            atomicAdd(&histogram[gray_image[tid]], 1);
+        }
     }
 
-
-    static unsigned char correct_color(const float cdf_val, const float cdf_min)
+    void compute_cdf(const int* histogram, float* cdf, int size)
     {
-        return static_cast<unsigned char>(255 * (cdf_val - cdf_min) / (1 - cdf_min));
+        int* d_temp_storage = nullptr;
+        size_t temp_storage_bytes = 0;
+
+
+        cub::DeviceScan::InclusiveSum(d_temp_storage, temp_storage_bytes, histogram, cdf, HISTOGRAM_LENGTH);
+        cudaMalloc(&d_temp_storage, temp_storage_bytes);
+        cub::DeviceScan::InclusiveSum(d_temp_storage, temp_storage_bytes, histogram, cdf, HISTOGRAM_LENGTH);
+        cudaFree(d_temp_storage);
+
+/*
+        thrust::transform(thrust::device, cdf, cdf + HISTOGRAM_LENGTH, cdf, [size] __device__ (float x)
+        {
+            return x / size;
+        });
+        */
     }
 
-
-    static void histogram_equalization(const int width, const int height,
-                                       const float* input_image_data,
-                                       float* output_image_data,
-                                       const std::shared_ptr<unsigned char[]>& uchar_image,
-                                       const std::shared_ptr<unsigned char[]>& gray_image,
-                                       int (&histogram)[HISTOGRAM_LENGTH],
-                                       float (&cdf)[HISTOGRAM_LENGTH]
-    )
+    __global__ void equalization_kernel(const unsigned char* input_image_data, float* output_image_data,
+                                        const float* cdf, float cdf_min, int size)
     {
-        return;
+        int tid = threadIdx.x + blockIdx.x * blockDim.x;
+        if (tid < size)
+        {
+            output_image_data[tid] = 255.0f * (cdf[input_image_data[tid]] - cdf_min) / (1.0f - cdf_min);
+        }
+    }
+
+    template <typename T>
+    __global__ void convert_to_gray_kernel(const T* input_image_data, unsigned char* gray_image, int size)
+    {
+        int tid = threadIdx.x + blockIdx.x * blockDim.x;
+        if (tid < size)
+        {
+            gray_image[tid] = 0.299f * input_image_data[3 * tid] + 0.587f * input_image_data[3 * tid + 1] + 0.114f *
+                input_image_data[3 * tid + 2];
+        }
     }
 
     wbImage_t iterative_histogram_equalization(const wbImage_t& input_image, const int iterations)
     {
-        // Input 1 2 5 4 9 7 0 1
-        // Output: 1 3 8 12 21 28 28 29
-
         const auto width = wbImage_getWidth(input_image);
         const auto height = wbImage_getHeight(input_image);
         constexpr auto channels = 3;
         const auto size = width * height;
         const auto size_channels = size * channels;
 
-        const wbImage_t output_image = wbImage_new(width, height, channels);
+        wbImage_t output_image = wbImage_new(width, height, channels);
         float* input_image_data = wbImage_getData(input_image);
         float* output_image_data = wbImage_getData(output_image);
 
-        std::shared_ptr<unsigned char[]> uchar_image(new unsigned char[size_channels]);
         std::shared_ptr<unsigned char[]> gray_image(new unsigned char[size]);
-
         int histogram[HISTOGRAM_LENGTH];
         float cdf[HISTOGRAM_LENGTH];
 
+        // Device arrays
+        unsigned char* d_gray_image;
+        int* d_histogram;
+        float* d_cdf;
+        float* d_input_image_data;
+        float* d_output_image_data;
+
+
+        cudaMalloc((void**)&d_gray_image, size * sizeof(unsigned char));
+        cudaMalloc((void**)&d_histogram, HISTOGRAM_LENGTH * sizeof(int));
+        cudaMalloc((void**)&d_cdf, HISTOGRAM_LENGTH * sizeof(float));
+        cudaMalloc((void**)&d_input_image_data, size_channels * sizeof(float));
+        cudaMalloc((void**)&d_output_image_data, size_channels * sizeof(float));
+
+        cudaMemcpy(d_input_image_data, input_image_data, size_channels * sizeof(float), cudaMemcpyHostToDevice);
+
         for (int i = 0; i < iterations; i++)
         {
-            histogram_equalization(width, height,
-                                   input_image_data, output_image_data,
-                                   uchar_image, gray_image,
-                                   histogram, cdf);
+            convert_to_gray_kernel<<<(size + 255) / 256, 256>>>(d_input_image_data, d_gray_image, size);
+            cudaDeviceSynchronize();
 
-            input_image_data = output_image_data;
+
+            cudaMemset(d_histogram, 0, HISTOGRAM_LENGTH * sizeof(int));
+            histogram_kernel<<<(size + 255) / 256, 256>>>(d_gray_image, size, d_histogram);
+            cudaDeviceSynchronize();
+
+
+            compute_cdf(d_histogram, d_cdf, size);
+
+            // Copy CDF from device to host
+            cudaMemcpy(cdf, d_cdf, HISTOGRAM_LENGTH * sizeof(float), cudaMemcpyDeviceToHost);
+
+
+            float cdf_min = cdf[0];
+            for (int j = 1; j < HISTOGRAM_LENGTH; j++)
+            {
+                if (cdf[j] < cdf_min)
+                {
+                    cdf_min = cdf[j];
+                }
+            }
+
+
+            equalization_kernel<<<(size + 255) / 256, 256>>>(d_gray_image, d_output_image_data, d_cdf, cdf_min, size);
+            cudaDeviceSynchronize();
+
+
+            cudaMemcpy(d_input_image_data, d_output_image_data, size_channels * sizeof(float),
+                       cudaMemcpyDeviceToDevice);
         }
+
+        cudaMemcpy(output_image_data, d_output_image_data, size_channels * sizeof(float), cudaMemcpyDeviceToHost);
+
+
+        cudaFree(d_gray_image);
+        cudaFree(d_histogram);
+        cudaFree(d_cdf);
+        cudaFree(d_input_image_data);
+        cudaFree(d_output_image_data);
 
         return output_image;
     }
--- test/histogram_eq_test.cpp
+++ test/histogram_eq_test.cpp
@@ -7,10 +7,11 @@ using namespace cp;
 #define DATASET_FOLDER "../../dataset/"
 #define OUTPUT_FOLDER "../../dataset_output/"
 
-#define TEST_ITERATIONS 10
-#define BIG_INPUT_TEST_ITERATIONS 3
+#define TEST_ITERATIONS 1
+#define BIG_INPUT_TEST_ITERATIONS 1
 
-TEST(HistogramEq, input01_org)
+
+TEST(HistogramEq, input01_omp)
 {
     int avg = 0;
     for (int i = 0; i < TEST_ITERATIONS; i++)
@@ -22,14 +23,14 @@ TEST(HistogramEq, input01_org)
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         avg += int(duration.count());
-        if (i == TEST_ITERATIONS - 1) { wbExport(OUTPUT_FOLDER "output01_org.ppm", outputImage); }
+        if (i == TEST_ITERATIONS - 1) { wbExport(OUTPUT_FOLDER "output01_omp.ppm", outputImage); }
     }
     avg /= TEST_ITERATIONS;
-    std::cout << "Execution time for " << TEST_ITERATIONS << " iterations in Original Function was: " << avg <<
+    std::cout << "Execution time for " << TEST_ITERATIONS << " iterations in OpenMP Function was: " << avg <<
         " microseconds" << std::endl;
 }
 
-TEST(HistogramEq, big_input_org)
+TEST(HistogramEq, big_input_omp)
 {
     int avg = 0;
     for (int i = 0; i < BIG_INPUT_TEST_ITERATIONS; i++)
@@ -41,10 +42,10 @@ TEST(HistogramEq, big_input_org)
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         avg += int(duration.count());
-        if (i == BIG_INPUT_TEST_ITERATIONS - 1) { wbExport(OUTPUT_FOLDER "big_output_org.ppm", outputImage); }
+        if (i == BIG_INPUT_TEST_ITERATIONS - 1) { wbExport(OUTPUT_FOLDER "big_output_omp.ppm", outputImage); }
     }
     avg /= BIG_INPUT_TEST_ITERATIONS;
-    std::cout << "Execution time for " << BIG_INPUT_TEST_ITERATIONS << " iterations in Original Function was: " << avg
-        <<
+    std::cout << "Execution time for " << BIG_INPUT_TEST_ITERATIONS << " iterations in OpenMP Function was: " << avg <<
         " microseconds" << std::endl;
 }
+
